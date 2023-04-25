e593201ea4b59dc358f7dbbd79a2219af1dc8db1
UGyeong Song <52400998+sugsugsug@users.noreply.github.com>
2023-04-25 15:47:15 -0400

    Update machine.go

--- machine.go
+++ machine.go
@@ -7,6 +7,54 @@ import (
 
 type Machine int
 
+type OperationID struct {
+        pid     int
+        pun     int
+}
+
+type Del struct {
+        pid     int
+        pun     int
+        l       *Node
+        r       *Node
+        undo
+        rendered
+}
+
+type Undo struct {
+        pid     int
+        pun     int
+        undo    *Undo
+        rendered bool
+}
+
+type Depl struct {
+        lpid    int
+        lpun    int
+        loffset int
+}
+type Depr struct {
+        rpid    int
+        rpun    int
+        roffset int
+}
+
+type Node struct {
+        pid     int
+        pun     int
+        offset  int
+        str     string
+        dels    []Del
+        undo    Undo // undo of insertion, nil if insertion is not undone
+        rendered bool
+        l       *Node
+        r       *Node
+        il      *Node
+        ir      *Node
+        depl    Depl
+        depr    Depr
+}
+
 type Node struct {
 }
 
